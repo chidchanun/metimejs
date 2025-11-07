160fb3b98cb2be0af37e4e259cf83140e58ae60e
chidchanun <chidchanun.bank@gmail.com>
2025-11-07 16:15:15 +0700

    Update API

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -2,50 +2,14 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { VerifyToken } from "@/app/utils/VerifyToken";
 
 export default function ProtectedPage() {
     const router = useRouter();
 
     useEffect(() => {
-        const verifyToken = async () => {
-            try {
-                // อ่าน token จาก cookie
-                const tokenCookie = document.cookie
-                    .split("; ")
-                    .find((row) => row.startsWith("auth_token=")); // ✅ ตรงกับที่เซิร์ฟเวอร์ตั้งไว้
+        VerifyToken()
 
-                if (!tokenCookie) {
-                    console.log("❌ ไม่พบ token");
-                    router.push("/login");
-                    return;
-                }
-
-                const token = decodeURIComponent(tokenCookie.split("=")[1]); // ✅ เผื่อ token มีอักขระพิเศษ
-
-                // ตรวจสอบ token ผ่าน API
-                const res = await fetch("/api/v1/token/verify", {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({ token }),
-                });
-
-                const data = await res.json();
-                console.log(data)
-
-                if (res.status !== 200) {
-                    console.warn("Token ไม่ถูกต้อง:", data.message);
-                    router.push("/login");
-                    return;
-                }
-
-                console.log("✅ Token ยังไม่หมดอายุ:", data.newExpiresAt);
-            } catch (err) {
-                console.error("เกิดข้อผิดพลาด:", err);
-                router.push("/login");
-            }
-        };
-
-        verifyToken();
     }, [router]);
 
     return (
