1ae29ce76de2e4214962fc34f511dd407cdd0d8f
bankbank123 <chidchanun.bank@mail.kmutt.ac.th>
2025-11-20 22:31:56 +0700

    ssssss

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+"use client";
 import Image from "next/image";
 import ChatComponent from "./components/ChatComponent";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+      window.location.href = "/login"
+  }, [])
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
